8e1071bed4d2294bbf71df5ee1b37083768cca04
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2023-02-11 15:13:50 +0530

    completed: finalizing the project and resolving the minor bug-fixes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import 'typeface-roboto';
 import registerServiceWorker from './registerServiceWorker';
-//import Home from './screens/Home/Home'
 import Controller from './screens/Controller';
-//import Details from './screens/details/Details';
 
 ReactDOM.render(<Controller />, document.getElementById('root'));
 registerServiceWorker();
